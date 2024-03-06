7ee170f8da046062f7e2b8b9bf40a5532a7f8790
Eugenio EJ <eugenioelzj@gmail.com>
2024-03-06 03:00:01 -0600

    Color correction

--- components/overview.tsx
+++ components/overview.tsx
@@ -71,7 +71,7 @@ export function Overview() {
           axisLine={false}
           tickFormatter={(value) => `$${value}`}
         />
-        <Bar dataKey="total" fill="#93CFF3" radius={[4, 4, 0, 0]} />
+        <Bar dataKey="total" fill="#0032ff" radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   );
