d7e4a8af64724c215d481fbc8e6243dc78543cbe
appp96 <appp@app.com>
2024-03-26 01:27:47 -0600

    instalación de prisma

--- app/(dashboard)/dashboard/taller/page.tsx
+++ app/(dashboard)/dashboard/taller/page.tsx
@@ -9,6 +9,7 @@ export default function page() {
       <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
         <BreadCrumb items={breadcrumbItems} />
         <UserClient data={taller} />
+        
       </div>
     </>
   ); 
